f7d375605597b1eadcbc1c1df810bbcf6c68486f
Hansel <lopezhansel@gmail.com>
2015-10-10 12:45:35 -0600

    started server and linked to html

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', {
+		root: './public'
+	});
+});
+
+
+var port = 3001;
+app.listen(port, function() {
+	console.log('started Lopezhansel.com on port: ', port);
+});
