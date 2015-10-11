5d4f2186ae8b67beab02535a4b4be8cea90576e0
Hansel <lopezhansel@gmail.com>
2015-10-10 19:11:19 -0600

    Getting ready to Start My profile

--- public/app/mainModule.js
+++ public/app/mainModule.js
@@ -1,2 +1,16 @@
-var app = angular.module('lopezhansel', []);
+var app = angular.module('lopezhansel', ['ngAnimate','ngRoute']);
 
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', { 
+      controller: 'homeCtrl', 
+      templateUrl: './app/home/home.html' 
+    })
+     .when('/mapMe', { 
+      controller: 'SubheaderAppCtrl', 
+      templateUrl: './app/mapMe/mapMe.html' 
+    })
+    .otherwise({ 
+      redirectTo: '/' 
+    });     
+});
