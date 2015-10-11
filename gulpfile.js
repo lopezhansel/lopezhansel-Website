08aa5a2e43f482c9d8a7c9e97a1da32f2b7838a4
Hansel <lopezhansel@gmail.com>
2015-10-11 13:28:08 -0600

    playing with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,65 @@
-var gulp = require('gulp');
+// // PLAYING WITH UGLIFY
+// var gulp = require('gulp')
+// 	uglify = require('gulp-uglify');
 
-gulp.task('default',function(){
-	console.log('hello')
-	});
+
+
+// gulp.task('scripts',function(){
+// 	// console.log('hello');
+// 	gulp.src('js/*.js')
+// 	.pipe(ugligy())
+// 	.pipe(gulp.dest('/build/js'));
+// });
+
+// gulp.task('styles',function  () {
+// 	console.log('running styles');
+// });
+	
+// gulp.task('default',['scripts','styles']);
+// -----------------------------------------------------------
+// // PLAYING WITH Watch
+// var gulp = require('gulp')
+// 	uglify = require('gulp-uglify');
+
+
+
+// gulp.task('scripts',function(){
+// 	// console.log('hello');
+// 	gulp.src('js/*.js')
+// 	.pipe(ugligy())
+// 	.pipe(gulp.dest('/build/js'));
+// });
+
+// gulp.task('styles',function  () {
+// 	console.log('running styles');
+// });
+	
+// gulp.task('watch',function  () {
+// 	gulp.watch('js/*.js',['scripts'])
+// })
+
+// gulp.task('default',['scripts','styles','watch']);
+
+// ------------------------------------------------------------
+// PLAYING WITH Watch
+var gulp = require('gulp')
+	uglify = require('gulp-uglify');
+
+
+
+gulp.task('scripts',function(){
+	// console.log('hello');
+	gulp.src('js/*.js')
+	.pipe(ugligy())
+	.pipe(gulp.dest('/build/js'));
+});
+
+gulp.task('styles',function  () {
+	console.log('running styles');
+});
+	
+gulp.task('watch',function  () {
+	gulp.watch('js/*.js',['scripts']);
+})
+
+gulp.task('default',['scripts','styles','watch']);
