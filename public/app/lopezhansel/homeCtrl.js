5cdb972edf1f6fa0224ebb2482770f09dc1a6dbc
Hansel <lopezhansel@gmail.com>
2015-10-10 23:40:04 -0600

    Working on the home views

--- public/app/lopezhansel/homeCtrl.js
+++ public/app/lopezhansel/homeCtrl.js
@@ -1,4 +1,3 @@
 app.controller('homeCtrl', ['$scope', function ($scope) {
 	$scope.greeting = "Welcome to my Page";
 }]);
-
--- public/app/mainModule.js
+++ public/app/mainModule.js
@@ -1,14 +1,26 @@
-var app = angular.module('lopezhansel', ['ngAnimate','ngRoute']);
+var app = angular.module('lopezhansel', ['ngAnimate','ngRoute',"ngMaterial"]);
 
 app.config(function($routeProvider){
   $routeProvider
     .when('/', { 
       controller: 'homeCtrl', 
-      templateUrl: './app/home/home.html' 
+      templateUrl: './app/lopezhansel/homeView.html' 
     })
-     .when('/mapMe', { 
-      controller: 'SubheaderAppCtrl', 
-      templateUrl: './app/mapMe/mapMe.html' 
+     .when('/resume', { 
+      controller: 'homeCtrl', 
+      templateUrl: './app/lopezhansel/resumeView.html' 
+    })
+    .when('/home', { 
+      controller: 'homeCtrl', 
+      templateUrl: './app/lopezhansel/homeView.html' 
+    })
+     .when('/blog', { 
+      controller: 'homeCtrl', 
+      templateUrl: './app/lopezhansel/blogView.html' 
+    })
+     .when('/myWork', { 
+      controller: 'homeCtrl', 
+      templateUrl: './app/lopezhansel/myWorkView.html' 
     })
     .otherwise({ 
       redirectTo: '/' 
