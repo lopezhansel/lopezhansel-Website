0e7569fc787ea5c66305a7ed5b5f5df8141a80d4
Hansel <lopezhansel@gmail.com>
2015-11-13 21:12:13 -0700

    Injecting Other Modules and setting up routes for other views

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
-var app = angular.module('lopezHansel',[]); 
+var app = angular.module('lopezHansel',['ngAnimate', 'ngMaterial', 'ngRoute']); 
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      controller: 'mainController',
+      templateUrl: 'views/homeView.html'
+    })
+    .when('/resume', {
+      controller: 'mainController',
+      templateUrl: 'views/resumeView.html'
+    })
+    .when('/portfolio', {
+      controller: 'mainController',
+      templateUrl: 'views/portfolioView.html'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+});
