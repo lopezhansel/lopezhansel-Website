49488bb24b19104fc60cd32ce4915e83e890901c
Hansel <lopezhansel@gmail.com>
2015-11-13 21:38:53 -0700

    Adding md-toolbar and md-tabs to switch between views Added indexController

--- public/js/controllers/indexController.js
+++ public/js/controllers/indexController.js
@@ -1,10 +1,10 @@
-app.controller('mainController', ['$scope',  '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", 
+app.controller('indexController', ['$scope',  '$routeParams', '$mdMedia', '$mdDialog', '$mdToast', "$http", "$interval", "$location", "$timeout", 
 	function($scope, $routeParams, $mdMedia, $mdDialog, $mdToast, $http, $interval, $location, $timeout) {
 	$scope.redirect = function(urlStr) {
 		console.log("hello");
 		$location.path(urlStr);
 	};
 
-	$scope.hello = "hello world";
+
 		
 }])	;
